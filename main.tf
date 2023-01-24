f6747c8a0ac63f21f6fc10ddfc285720d0e95ccc
Li Lin <li.lin@hashicorp.com>
2023-01-24 14:35:48 -0800

    Add hostname var

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ variable "organization" {} ## Your TFC Organization
 variable "terraform_version" {} ## TFC version you would like to use(example: 0.12.26)
 variable "vcs_oauth_token_id" {} ## Your VCS Oauth token
 variable "vcs_repoorg" {}  ## Your VCS organization, where `different-repo` get forked
+variable "hostname" {}  ## Your TFE host name
 
 module "my_new_workspace" {
 
@@ -23,4 +24,8 @@ module "my_new_workspace" {
 
   trigger_prefixes = ["./module"]
 
+}
+
+provider "tfe" {
+  hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
 }
